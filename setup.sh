5808fd37365f59092b8baa2b8b9bd2cdfb6b9997
Edvard LUNDIN <edvard.lundin@pm.me>
2022-05-17 22:12:51 +0200

    feat: macos env
    Signed-off-by: Edvard Lundin <edlundin@macbookpro.local>

--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 echo "Updating system"
-sudo apt update
-sudo apt upgrade
+brew update
 
 echo "Installing dependencies..."
-sudo apt install fd-find exa bat zsh vim wget curl git fonts-firacode gnupg2 stow feh
+brew install fd exa bat zsh vim wget curl git gnupg2 stow feh
+brew tap homebrew/cask-fonts
+brew install font-fira-code --cask
 
 echo "Installing vim plugins..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -15,16 +16,16 @@ git clone --quiet https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightli
 ln -nfs "$SCRIPT_DIR"/vim/vimrc "$HOME"/.vimrc
 
 echo "Installing Zim..."
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+wget -nv -O - https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 ln -nfs "$SCRIPT_DIR"/zim/zlogin "$HOME"/.zlogin
 ln -nfs "$SCRIPT_DIR"/zim/zshrc "$HOME"/.zshrc
 ln -nfs "$SCRIPT_DIR"/zim/zimrc "$HOME"/.zimrc
 ln -nfs "$SCRIPT_DIR"/zim/zshenv "$HOME"/.zshenv
 ln -nfs "$SCRIPT_DIR"/zim/zprofile "$HOME"/.zprofile
 
-echo "Installing WSL2 SSH Pageant..."
-wget https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/download/v1.4.0/wsl2-ssh-pageant.exe -O ~/.ssh/wsl2-ssh-pageant.exe
-chmod +x ~/.ssh/wsl2-ssh-pageant.exe
 chsh -s /bin/zsh
+
+zsh ~/.zim/zimfw.zsh compile
+zsh ~/.zim/zimfw.zsh install 
 
 exit 0
